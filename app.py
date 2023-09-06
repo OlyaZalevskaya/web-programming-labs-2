5ed78bfac8053802d8e840fa8dd7355789eead3f
Ольга Залевская <Olga.Zalevskaya03@mail.ru>
2023-09-06 18:07:24 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Залевская Ольга Сергеевна, лабораторная 1</title>
+    </head>    
+    <body>
+        <header>
+        НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Залевская Ольга, ФБИ-14, 3 курс, 2023
+        </footer> 
+    </body>   
+</html>
+"""
