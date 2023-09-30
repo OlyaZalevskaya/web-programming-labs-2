c3bbd146c5839342cd11422e14bb939d4206cbf2
Ольга Залевская <Olga.Zalevskaya03@mail.ru>
2023-10-01 01:21:46 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -231,3 +231,7 @@ def example():
     return render_template('example.html', 
                            name=name, numer=numer, gruppa=gruppa, 
                            curs=curs, fruits=fruits, buch=buch)
+
+@app.route("/lab2/")
+def lab2():
+    return render_template('lab2.html')
