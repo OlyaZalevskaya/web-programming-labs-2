fe011480ff257651dbbbbbfb1afc667da26f00d5
Ольга Залевская <Olga.Zalevskaya03@mail.ru>
2023-10-15 21:38:05 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -21,3 +21,34 @@ def forml():
     
     sex = request.args.get('sex')
     return render_template('forml.html', user=user, age=age, sex=sex, errors=errors,error=error)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    # Пусть кофе стоит 120 рублей, черный чай - 80 рублей, зеленый - 70 рублей.
+    if drink == 'cofee':
+        price =120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    # Добавка молока удорожает напиток на 30 рублей, а сахар - на 10.
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('pay.html', price = price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
