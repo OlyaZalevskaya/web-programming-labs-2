d8ab02b0ea11fb0ed69d22658f12fa9cd9b76123
Ольга Залевская <olga.zalevskaya03@mail.ru>
2023-10-31 22:51:31 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -26,3 +26,31 @@ def login():
         error = 'Неверный логин и/или пароль'
         return render_template('login.html', error=error, username=username, password=password)
     
+@lab4.route('/lab4/holodil', methods=['GET', 'POST'])
+def holodil():
+    if request.method == 'GET':
+        return render_template('holodil.html')
+    
+    temperature = request.form.get('temperature')
+    error = ''
+    message = ''  # Начальное значение для переменной message
+    snowflakes = ''  # Начальное значение для переменной snowflakes
+    
+    if temperature is None or temperature == '':
+        error = 'ошибка: не задана температура'
+    elif int(temperature) < -12:
+        error = 'не удалось установить температуру — слишком низкое значение'
+    elif int(temperature) > -1:
+        error = 'не удалось установить температуру — слишком высокое значение'
+    elif -12 <= int(temperature) <= -9:
+        message = f'Установлена температура: {temperature}°C'
+        snowflakes = '❄️❄️❄️'
+    elif -8 <= int(temperature) <= -5:
+        message = f'Установлена температура: {temperature}°C'
+        snowflakes = '❄️❄️'
+    elif -4 <= int(temperature) <= -1:
+        message = f'Установлена температура: {temperature}°C'
+        snowflakes = '❄️'
+    
+    return render_template('holodil.html', error=error, temperature=temperature, message=message, snowflakes=snowflakes)
+    
