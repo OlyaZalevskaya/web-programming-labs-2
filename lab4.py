57be69cb1ee688da03cb613c9ea22e67e27656e6
Ольга Залевская <olga.zalevskaya03@mail.ru>
2023-11-01 13:57:34 +0700

    Куки-цвет текста

--- lab4.py
+++ lab4.py
@@ -91,3 +91,15 @@ def zerno():
  
     return render_template('zerno.html', error=error, zerno=zerno, ves=ves, message=message) 
  
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST']) 
+def cookies(): 
+    if request.method == 'GET': 
+        return render_template('cookies.html') 
+ 
+    color = request.form.get('color') 
+    headers = { 
+        'Set-Cookie': 'color=' + color + ' ; path=/', 
+        'Location': '/lab4/cookies' 
+    } 
+    return '', 303, headers 
+    
