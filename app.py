82c2457f325219ef62215e3e2ee001ec5fb7c425
Ольга Залевская <olga.zalevskaya03@mail.ru>
2023-11-29 01:16:54 +0700

    Подключение ORM к проекту

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from Db import db
+from Db.models import users, articles
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = 'password'
+user_db = 'zalevskaya_olga_knowledge_base_orm'
+host_ip = '127.0.0.1'
+host_port = '5432'
+database_name = 'knowledge_base_for_zalevskaya_olga_orm'
+password = '123'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
--- lab6.py
+++ lab6.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
-lab4 = Blueprint('lab6', __name__)
+lab6 = Blueprint('lab6', __name__)
 
-@lab4.route('/lab6/')
+@lab6.route('/lab6/')
 def lab():
     return render_template('lab6.html')
