6d495cb445fed65b9f14179c19abbd33d553937a
Ольга Залевская <olga.zalevskaya03@mail.ru>
2023-11-29 02:59:09 +0700

    Адаптация лабораторной работы No5 к системе ORM

--- lab6.py
+++ lab6.py
@@ -1,6 +1,22 @@
-from flask import Blueprint, render_template
+from flask import redirect, Blueprint, render_template, request
+from Db import db
+from Db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user, logout_user
+from flask import url_for
+
 lab6 = Blueprint('lab6', __name__)
 
-@lab6.route('/lab6/')
-def lab():
-    return render_template('lab6.html')
+
+@lab6.route("/lab6/check")
+def main():
+    my_users = users.query.all()
+    print(my_users)
+    return "result in console!"
+
+
+@lab6.route("/lab6/checkarticles")
+def checkarticles():
+    all_articles = articles.query.all()
+    print(all_articles)
+    return "result in console!"
