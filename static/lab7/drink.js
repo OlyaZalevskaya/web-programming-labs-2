a387fff9bf7f7493729cca4257687d45c204f73f
Ольга Залевская <olga.zalevskaya03@mail.ru>
2023-12-13 19:48:33 +0700

    Правки

--- static/lab7/drink.js
+++ static/lab7/drink.js
@@ -1,7 +1,7 @@
 function getPrice() {
     const milk = document.querySelector('[name=milk]').checked;
     const sugar = document.querySelector('[name=sugar]').checked;
-    const drink = document.querySelector('[name=drink]:checked') ? document.querySelector('[name=drink]:checked').value : '';
+    const drink = document.querySelector('[name=drink]:checked').value;
 
     const obj = {
         "method": "get-price",
