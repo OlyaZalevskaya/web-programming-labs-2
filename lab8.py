41f2c5d23a2bdb62a1c61c37428e71350617a8df
Ольга Залевская <olga.zalevskaya03@mail.ru>
2023-12-19 20:09:59 +0700

    Получение информации о курсе/-ах по REST

--- lab8.py
+++ lab8.py
@@ -8,3 +8,27 @@ lab8 = Blueprint('lab8',__name__)
 def main():
     return render_template('lab8/index.html')
 
+
+courses = [
+    {"name": "c++", "videos": 3, "price": 3000, "created_date": datetime.now()},
+    {"name": "basic", "videos": 30, "price": 450, "created_date": datetime.now()},
+    {"name": "python", "videos": 15, "price": 1450, "created_date": datetime.now()},
+    {"name": "c#", "videos": 8, "created_date": datetime.now()}
+]
+
+
+#Здесь мы определяем, что у нас будет специальный путь, по которому будет
+#"жить" API
+#здесь список курсов
+@lab8.route('/lab8/api/courses/', methods=['GET'])
+def get_courses():
+    return jsonify(courses)
+
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        abort(404)  # возвращаем ошибку 404, если номер выходит за пределы
+    return courses[course_num]
+
+
